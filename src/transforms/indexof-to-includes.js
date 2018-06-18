90cb692c3a67c27547c48772944383f0b66b0f1f
ajhorst <ajhorst@gmail.com>
2018-06-17 22:24:23 -0700

    set up outline of code

---
+++ src/transforms/indexof-to-includes.js
@@ -0,0 +1,8 @@
+module.exports = (file, api, options) => {
+    const j = api.jscodeshift;
+
+    const printOptions = options.printOptions || {quote: 'single'};
+    const root = j(file.source);
+
+    return root.toSource(printOptions);
+};
